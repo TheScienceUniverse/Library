01d216fe30a29cf92c633539d6abb45834eaad04
Sayan Shankhari <34585895+Sayan0Programmer@users.noreply.github.com>
2018-09-05 13:33:33 +0530

    Add files via upload

--- Go/myNum/myNum.go
+++ Go/myNum/myNum.go
@@ -15,10 +15,22 @@ func gcd(a, b int) int {
 	}
 	return a;
 }
-
+func PreProcess(n *Num) {
+	if n.N == 0 {
+		n.D = 1
+	} else if n.D == 0 {
+		n.N = 1
+	}
+	if(n.N & n.D) != 0 {
+		x := gcd(n.N, n.D)
+		n.N /= x; n.D /= x
+	}
+	if n.D < 0 {
+		n.N *= -1; n.D *= -1
+	}
+}
 func Calc(a, b Num, s byte) Num {
 	var n Num
-	var x int
 	if s == '+'{
 		n.N = b.D * a.N + a.D * b.N
 		n.D = a.D * b.D
@@ -32,7 +44,8 @@ func Calc(a, b Num, s byte) Num {
 		n.N = a.N * b.D
 		n.D = a.D * b.N
 	}
-	if n.N == 0 { n.D = 1 }
+	PreProcess(&n)
+/*	if n.N == 0 { n.D = 1 }
 	if (n.N & n.D) != 0{
 		x = gcd(n.N, n.D)
 		n.N /= x; n.D /= x
@@ -40,9 +53,10 @@ func Calc(a, b Num, s byte) Num {
 	if n.D < 0 {
 		n.N *= -1
 		n.D *= -1
-	}
+	}*/
 	return n
 }
+
 func MinI(A []Num, n int) int {
 	s := A[0]
 	var p int = 0
@@ -54,4 +68,3 @@ func MinI(A []Num, n int) int {
 	}
 	return p
 }
-
