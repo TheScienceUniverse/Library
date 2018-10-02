978d94cc04e6ce49274d8a11e9cefdaeb3f1e295
Sayan Shankhari <34585895+Sayan0Programmer@users.noreply.github.com>
2018-10-02 23:21:25 +0530

    Add files via upload

--- C/sort/bubble.h
+++ C/sort/bubble.h
@@ -3,6 +3,13 @@
 #include "myAlgo.h"
 void sort(int *A, int n) {
 	int i, j;
+/*
+	for (i = 0; i < n-1; i++) {
+		for (j = 0; j < n-1; j++)
+			if (A[j] > A[j+1])
+				swap(&A[j], &A[j+1]);
+	}
+ */
 	for (i = 0; i < n-1; i++) {
 		for (j = 0; j < n-i-1; j++)
 			if (A[j] > A[j+1])
